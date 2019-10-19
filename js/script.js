19a1abd5d3648520e6512dc393d149cafe2157ef
H3r3sJohnny <nikpomazkin@gmail.com>
2019-10-19 22:11:45 +0300

    Добавлена функция выводящая дерево на экран

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 //Функция для получения данных из json
 async function getJson() {
 const response = await fetch('https://raw.githubusercontent.com/wrike/frontend-test/master/data.json');
-const myJson = await response.json();
-return myJson;
+return await response.json();
 }
 
 //Создание массива дерева
@@ -19,7 +18,7 @@ function CreateTree(data) {
         for(id of tree.keys()){
             let folder = tree.get(id);
             let parentId = folder.parentId;
-            if(folder.parentId == i){
+            if(folder.parentId === i){
                 tree.get(parentId).children.push(folder);
             }
         }
@@ -35,12 +34,49 @@ function CreateTree(data) {
     }
 
 }
+//Функция отрисовывающая массив на странице
+function printTree(tree) {
+    document.getElementById('tree').innerHTML = '';
+    for (let elem of tree.treeObject) {
+        document.querySelector(`#tree`).appendChild(createElement(elem));
+        if (elem.children.length !== 0){
+            printChild(elem);
+        }
+    }
+    for (const node of document.getElementsByClassName("node-label")) {
+        node.addEventListener("click", toggleNode);
+        if (node.closest(".node").children.length - 1 === 0) node.classList.add("node-label_endpoint");
+    }
+//Функция рисующая дочерние элементы дерева
+function printChild(element){
+    this.elem = element.children;
+    for (let id of this.elem) {
+        if (id.children.length !== 0) {
+            document.querySelector(`#tree`).appendChild(createElement(id));
+            printChild(id);
+        }
+        else {
+            document.querySelector(`#tree`).appendChild(createElement(id));
+        }
+    }
+}
+
+//Функция преобразует элемент массива в элемент отображения
+function createElement(elem){
+    let element = document.createElement("div");
+    let label = document.createElement("span");
+    label.innerText = elem.title;
+    element.appendChild(label);
+    return element;
+    }
+
+
+}
 
 
 async function start() {
     let data = await getJson();
     let tree = new CreateTree(data);
-    console.log(tree);
-
+    printTree(tree);
 }
 start();
